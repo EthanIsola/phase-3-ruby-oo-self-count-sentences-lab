945d0475a34d170bd5d5512b7a108689f20bd7bd
Ethan Isola <isolaethan@gmail.com>
2022-02-23 15:01:56 -0600

    commit tree 4c616054c2c385dabb25bb6c28b1541711d32f9f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    puts self
+    letter_arr = self.split("")
+    count = 0
+    temp = ""
+    letter_arr.each do |letter|
+      if (letter === "?")&&(letter != temp)
+        count = count + 1
+      elsif (letter === ".")&&(letter != temp)
+        count = count + 1
 
+      elsif (letter === "!")&&(letter != temp)
+        count = count + 1
+      end
+      temp = letter
+    end
+    count
   end
 end
